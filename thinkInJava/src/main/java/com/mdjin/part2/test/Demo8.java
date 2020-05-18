e7eae26277eb4115597513d9b62fd21f31d16709
jin maodong <jinmaodong@outlook.com>
2020-05-19 00:31:17 +0800

    add project-spring & update project thinkinjava

---
+++ thinkInJava/src/main/java/com/mdjin/part2/test/Demo8.java
@@ -0,0 +1,11 @@
+package com.mdjin.part2.test;
+
+/**
+ * 第二章>练习8
+ *  编写一个程序，展示无论你创建了某个特定类的多少各对象，这个类中的某个特定的static域只有一个实例。
+ * @author jinmaodong
+ * @date 2020/5/19
+ * @since 1.0.0
+ **/
+public class Demo8 {
+}
