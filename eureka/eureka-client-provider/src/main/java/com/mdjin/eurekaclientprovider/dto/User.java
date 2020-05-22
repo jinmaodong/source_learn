34597bd8fb0b90373f49d2bc7c24368b7c48d16f
jinmaodong <jinmaodong@outlook.com>
2020-05-23 01:09:16 +0800

    fixed untracked files

--- eureka/eureka-client-provider/src/main/java/com/mdjin/eurekaclientprovider/dto/User.java
+++ eureka/eureka-client-provider/src/main/java/com/mdjin/eurekaclientprovider/dto/User.java
@@ -1,6 +1,8 @@
 package com.mdjin.eurekaclientprovider.dto;
 
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,21 +14,25 @@ import lombok.Setter;
  **/
 @Getter
 @Setter
+@ApiModel(value = "用户信息接口-接口传输类",description = "参数-user")
 public class User {
 
     /**
      * id
      */
+    @ApiModelProperty(name = "用户ID",value = "32位字符串，符合驼峰规则")
     private String id;
 
     /**
      * name
      */
+    @ApiModelProperty(value = "用户名称")
     private String name;
 
     /**
      * age
      */
+    @ApiModelProperty(value = "年龄",required = true)
     private int age;
 
 
